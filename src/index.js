211a79492c59678cbaa8da6a6a635603919dcda2
Jessica Slusark <slusark.jessica@gmail.com>
2022-11-22 17:23:13 +0100

    Added predifined temp and fixed a bit of transition from c to f and viceversa

--- src/index.js
+++ src/index.js
@@ -1,44 +1,106 @@
-// Change degrees from Celsius to Fahrenheit and vice versa
-let mainTempertature = document.querySelector("#mainTemp");
-let fahrenight = document.querySelector("#F");
-fahrenight.addEventListener("click", fTemperature);
-let celsius = document.querySelector("#C");
-celsius.addEventListener("click", cTemperature);
+// nextdays
 let temp1 = document.querySelector("#t1");
 let temp2 = document.querySelector("#t2");
 let temp3 = document.querySelector("#t3");
 let temp4 = document.querySelector("#t4");
 let temp5 = document.querySelector("#t5");
 let temp6 = document.querySelector("#t6");
+let cityTitle = document.querySelector("#citytitle");
+let mainTempertature = document.querySelector("#mainTemp");
 console.log("check below");
-console.log(mainTempertature.innerHTML);
-console.log(temp1.innerHTML + 1);
+console.log(mainTempertature.innerHTML + cityTitle.innerHTML);
+let fahrenight = document.querySelector("#F");
+let celsius = document.querySelector("#C");
 
-function fTemperature(F) {
-  mainTempertature.innerHTML = Math.round(
-    (mainTempertature.innerHTML * 9) / 5 + 32
-  );
-  temp1.innerHTML = Math.round((temp1.innerHTML * 9) / 5 + 32);
-  temp2.innerHTML = Math.round((temp2.innerHTML * 9) / 5 + 32);
-  temp3.innerHTML = Math.round((temp3.innerHTML * 9) / 5 + 32);
-  temp4.innerHTML = Math.round((temp4.innerHTML * 9) / 5 + 32);
-  temp5.innerHTML = Math.round((temp5.innerHTML * 9) / 5 + 32);
-  temp6.innerHTML = Math.round((temp6.innerHTML * 9) / 5 + 32);
+//show predefined temperature
+function predefinedTemperature (berlinTemperature) {
+  let apiKey = "f5029b784306910c19746e40c14d6cd3";
+  let apiGeocode = `https://api.openweathermap.org/data/2.5/weather?q=${cityTitle.innerHTML}&appid=${apiKey}`;
+  axios.get(`${apiGeocode}&&units=metric`).then(showTemperature);}
+ 
+  predefinedTemperature();
+
+  // City search
+
+function citySearch(event) {
+  event.preventDefault();
+  let tipedCity = document.querySelector("#city"); //h2select
+  let writtenCity = tipedCity.value.trim(); //what has been written in H2
+  let finalCity =
+    writtenCity.charAt(0).toUpperCase() + writtenCity.slice(1).toLowerCase();
+  cityTitle.innerHTML = finalCity;
+
+  console.log(tipedCity)
+  console.log(writtenCity)
+    console.log(finalCity)
+    
+// weather APIs
+    let apiKey = "f5029b784306910c19746e40c14d6cd3";
+    let apiGeocode = `https://api.openweathermap.org/data/2.5/weather?q=${finalCity}&appid=${apiKey}`;
+    axios.get(`${apiGeocode}&&units=metric`).then(cityTemperature)
+
+    // gives temperature when writing a new city 
+    function cityTemperature(response) {
+      let searchedCityTemp = Math.floor(response.data.main.temp)
+      console.log(finalCity + " " + searchedCityTemp)
+      //change main temperature
+      mainTempertature.innerHTML = searchedCityTemp
+    
+    }
+  }
+
+
+let form = document.querySelector("#citysearch");
+form.addEventListener("submit", citySearch);
+
+
+//location button detectslocation and then calls showTemperature function 👆)
+function detectLocation(location) {
+  navigator.geolocation.getCurrentPosition(myPosition);
+
+  function myPosition(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+  console.log(`lat:${lat} lon:${lon}`);
+    let apiKey = "f5029b784306910c19746e40c14d6cd3";
+    let apiGeocode = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    axios.get(`${apiGeocode}&&units=metric`).then(showTemperature);
+  }}
+
+let locationButton = document.querySelector("#locationicon")
+locationButton.addEventListener("click", detectLocation)
+
+
+//Show temperature of typed or geolocated city
+function showTemperature(celsius) {
+  let locationTemp = Math.floor(celsius.data.main.temp);
+  let detectedLocationName = celsius.data.name;
+  console.log(`detectedLocationName:${detectedLocationName} locationTemp:${locationTemp}`);
+  mainTempertature.innerHTML = locationTemp;
+  cityTitle.innerHTML = detectedLocationName;
+}
+
+
+
+
+
+function cTemperature(cNumber) {
+  mainTempertature.innerHTML = Math.round((mainTempertature.innerHTML-32)* 5/9 );
+  celsius.style.color = "#ffffff";
+  fahrenight.style.color = "#ffffff30";
+celsius.style.noHover = true;
+}
+celsius.addEventListener("click", cTemperature);
+
+
+function fTemperature(fNumber) {
+  mainTempertature.innerHTML = Math.round((mainTempertature.innerHTML * 9) / 5 + 32);
+  console.log(mainTempertature.innerHTML)
   celsius.style.color = "#ffffff30";
   fahrenight.style.color = "#ffffff";
 }
+fahrenight.addEventListener("click", fTemperature);
 
-function cTemperature(C) {
-  mainTempertature.innerHTML = "-1";
-  temp1.innerHTML = "20";
-  temp2.innerHTML = "18";
-  temp3.innerHTML = "12";
-  temp4.innerHTML = "17";
-  temp5.innerHTML = "17";
-  temp6.innerHTML = "17";
-  celsius.style.color = "#ffffff";
-  fahrenight.style.color = "#ffffff30";
-}
 
 // week and monthcycle array
 let week = [
@@ -80,75 +142,4 @@ let lineTwoRight = document.querySelector("#line2r");
 
 lineOneRight.innerHTML = `${day} ${month} ${year}`;
 lineTwoRight.innerHTML = `${weekday}, ${time}`;
-
-// City search
-
-function citySearch(event) {
-  event.preventDefault();
-  let tipedCity = document.querySelector("#city"); //h2select
-  let writtenCity = tipedCity.value.trim(); //what has been written in H2
-  let finalCity =
-    writtenCity.charAt(0).toUpperCase() + writtenCity.slice(1).toLowerCase();
-  cityTitle.innerHTML = finalCity;
-
-  console.log(tipedCity)
-  console.log(writtenCity)
-    console.log(finalCity)
-    
-    // gives temperature when writing a new city 
-    let apiKey = "f5029b784306910c19746e40c14d6cd3";
-    let apiGeocode = `https://api.openweathermap.org/data/2.5/weather?q=${cityTitle.innerHTML}&appid=${apiKey}`;
-    function cityTemperature(response) {
-      console.log(Math.floor(response.data.main.temp))
-      let searchedCityTemp = Math.floor(response.data.main.temp)
-      //change main temperature
-      mainTempertature.innerHTML = searchedCityTemp
-    
-    }
-    axios.get(`${apiGeocode}&&units=metric`).then(cityTemperature)
-  }
-
-let form = document.querySelector("#citysearch");
-form.addEventListener("submit", citySearch);
-
-let cityTitle = document.querySelector("#citytitle");
-console.log(cityTitle.innerHTML);
-
-
-
-//Geolocation button
-let locationButton = document.querySelector("#locationicon")
-function detectLocation(location) {
-  function myPosition(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    console.log(lat);
-    console.log(lon);
-  
-    let apiKey = "f5029b784306910c19746e40c14d6cd3";
-    let apiGeocode = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    axios.get(`${apiGeocode}&&units=metric`).then(myTemperature);
-
-   
-  }
-  navigator.geolocation.getCurrentPosition(myPosition);
-}
-
-//Show temperature of typed city
-function myTemperature(response) {
-  console.log(Math.floor(response.data.main.temp));
-  let locationTemp = Math.floor(response.data.main.temp);
-  mainTempertature.innerHTML = locationTemp;
-  console.log(response.data.name);
-  let detectedLocationName = response.data.name;
-  cityTitle.innerHTML = detectedLocationName;
-  if (detectedLocationName === "Land Berlin") {   cityTitle.innerHTML = "Berlin";
-
-
-    
-  }
-
-}
-
-locationButton.addEventListener("click", detectLocation)
 
